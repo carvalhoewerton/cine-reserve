4f231fbcc6112dd45aad47747e505f9083f5ca1e
Ewerton <eww999@hotmail.com>
2026-03-22 20:27:21 -0300

    feat: project setup

---
+++ setup/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+
+urlpatterns = [
+    path('', include('apps.user.urls')),
+    path('', include('apps.movie.urls')),
+    path('', include('apps.room.urls')),
+    path('', include('apps.session.urls')),
+    path('', include('apps.ticket.urls')),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+]
